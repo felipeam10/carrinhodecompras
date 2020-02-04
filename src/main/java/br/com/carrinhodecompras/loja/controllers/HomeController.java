8946012c839bbf9041105873624450bf53555f15
felipeam10 <felipeam10@hotmail.com>
2020-02-03 22:45:14 -0200

    teste que nao acaba mais

--- src/main/java/br/com/carrinhodecompras/loja/controllers/HomeController.java
+++ src/main/java/br/com/carrinhodecompras/loja/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+	/*teste*/
 	@RequestMapping("/")
 	public String index() {
 		System.out.println("Entrando na home do carrinho de compras");
